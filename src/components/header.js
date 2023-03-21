88b625085f2265986e4c0e006ed2dc255164de37
pranavhole <pranavhole050610@gmail.com>
2023-03-22 00:20:48 +0530

    'second'

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const header = () => {
     <div className=' bg-[#1E2A55] text-white font-bold py-2 px-4 flex justify-between'>
         <div className='max-w-2xl justify-center'>logo</div>
         <div className='flex justify-center static w-full'> 
-          <div className=' px-20 bg-slate-50 rounded-full text-[#216825] py-2 text-center'>Home</div>
+          <div className=' px-20 bg-slate-50 rounded-full text-[#216825] py-2 text-center text-2xl'>Home</div>
           </div>
     </div>
   )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,29 @@
 import React from 'react'
-
+import home from '../images/HomePage.png'
+import Dash from '../images/Dashboard.png'
+import Info from '../images/Info.png'
+import Course from '../images/Course.png'
+import Current from '../images/Current.png'
 const navbar = () => {
   return (
-    <div>
-        <ul>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-        </ul>
+    <div className='my-6 mx-2 bg-[#1E2A55] w-28 text-center rounded-3xl'>
+      <ul className=' justify-between inline-block'>
+        <li className=' items-center my-6 py-3'>
+          <img src={Dash} alt='Home' />
+          <span>Home</span></li>
+        <li className='items-center my-6 py-3'>
+          <img src={home} alt='Home' />
+          <span>Home</span></li>
+        <li className='items-center my-6 py-3'>
+          <img src={Course} alt='Home' />
+          <span>Home</span></li>
+        <li className='items-center my-6 py-3'>
+          <img src={Current} alt='Home' />
+          <span>Home</span></li>
+        <li className='items-center my-6 py-3'>
+          <img src={Info} alt='Home' />
+          <span>Home</span></li>
+      </ul>
     </div>
   )
 }
