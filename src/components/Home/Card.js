4835a32a8228bda46f5cc590252e2c7402cd6470
Arti Manputra <aartimanputra20@gmail.com>
2023-03-29 15:18:12 +0530

    Added course cards on home page

---
+++ src/components/Home/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="term">
+      <div className="top">
+        <img className="square-img" src={props.img} alt="avatar_img" />
+        <div className="title">{props.title}</div>
+      </div>
+      <div className="middle">
+        <p className="price">{props.price}</p>
+        <p className="offer">{props.offer}</p>
+        <a className="seeMore">See More +</a>
+      </div>
+      <div className="bottom">
+      <a>Subscribe Now</a>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
