d2731b14bbd4f19b6d6c99f891e607af85be97dc
pranavhole <pranavhole050610@gmail.com>
2023-03-30 11:02:35 +0530

    Home and navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,19 +10,22 @@ const navbar = () => {
   return (
     <div className='my-6 mx-2 bg-[#1E2A55] w-28 text-center rounded-3xl'>
     <ul className=' justify-between inline-block'>
-      <Link to="./dashboard"><li className=' items-center my-6 py-3'>
+      <Link to="./dashboard"><li className=' items-center my-3 '>
         <div className='inline-block'><img src={Dash} alt='Home' /></div>
         <div>Dashboard</div></li></Link>
-        <Link to="./"><li className='items-center my-6 py-3'>
+        <Link to="./"><li className='items-center my-3 '>
         <div className='inline-block'><img src={home} alt='Home' /></div>
         <div>Home</div></li></Link>
-        <Link to="./"><li className='items-center my-6 py-3'>
+        <Link to="./"><li className='items-center my-3 '>
         <div className='inline-block'><img src={Course} alt='Home' /></div>
         <div>Home</div></li></Link>
-        <Link to="./"><li className='items-center my-6 py-3'>
+        <Link to="./"><li className='items-center my-3 '>
         <div className='inline-block'><img src={Current} alt='Home' /></div>
         <div>Home</div></li></Link>
-        <Link to="./"><li className='items-center my-6 py-3'>
+        <Link to="./"><li className='items-center my-3 '>
+        <div className='inline-block'><img src={Profile} alt='Home' /></div>
+        <div>Home</div></li></Link>
+        <Link to="./"><li className='items-center my-3 '>
         <div className='inline-block'><img src={Info} alt='Home' /></div>
         <div>Home</div></li></Link>
     </ul>
