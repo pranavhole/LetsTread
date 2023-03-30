458997ec7065045d7794b48f1204dc1b64c77866
pranavhole <pranavhole050610@gmail.com>
2023-03-30 14:20:32 +0530

    final 30

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,6 +22,10 @@ const navbar = () => {
         <Link to="./"><li className='items-center my-3 '>
         <div className='inline-block'><img src={Current} alt='Home' /></div>
         <div>Home</div></li></Link>
+        <Link to="./"><li className='items-center my-3 '>
+        <div className='inline-block'><img src={Profile} alt='Home' /></div>
+        <div>Home</div></li></Link>
+        <Link to="./"><li className='items-center my-3 '>
         <div className='inline-block'><img src={Info} alt='Home' /></div>
         <div>Home</div></li></Link>
     </ul>
